3f023db686d78f677437a6a76ad7ffb8614d57aa
nguyenhuyhoang <huyhoang17012006@gmail.com>
2025-02-06 17:22:49 +0700

    fv

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function navigateTo(url) {
     window.location.href = url; // Navigate to the given URL
 }
+function validateNumberInput(event) {
+    const input = event.target;
+    input.value = input.value.replace(/[^0-9 ]/g, "");
+  }
+
