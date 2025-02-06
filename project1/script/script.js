fea417769663ab7d534e65d51abf785947dd77ec
vbphuong <vbphuong012@gmail.com>
2025-02-06 00:53:12 -0800

    init

---
+++ project1/script/script.js
@@ -0,0 +1,18 @@
+function navigateTo(url) {
+    window.location.href = url; // Navigate to the given URL
+}
+
+function search() {
+    var query = document.getElementById("favseason").value.toLowerCase();
+    var links = {
+        "google": "https://www.google.com",
+        "youtube": "https://www.youtube.com",
+        "facebook": "https://www.facebook.com"
+    };
+    
+    if (links[query]) {
+        window.location.href = links[query];
+    } else {
+        alert("No results found");
+    }
+}
