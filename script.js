87d24ce2db06bd7f616955e6a724d19622140d1f
vbphuong <vbphuong012@gmail.com>
2025-02-07 18:57:32 -0800

    doimaimotnguoi

--- script.js
+++
@@ -1,8 +0,0 @@
-function navigateTo(url) {
-    window.location.href = url; // Navigate to the given URL
-}
-function validateNumberInput(event) {
-    const input = event.target;
-    input.value = input.value.replace(/[^0-9 ]/g, "");
-  }
-
