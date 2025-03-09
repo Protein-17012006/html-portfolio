688b242e041eba7e86b3266795dfd98dac8e619b
vbphuong <vbphuong012@gmail.com>
2025-03-09 01:55:44 -0800

    doimaimotnguoi

--- project1/setting.php
+++ project1/setting.php
@@ -3,15 +3,4 @@ $host = "feenix-mariadb.swin.edu.au";
 $username = "s105710430"; 
 $password = "061206";
 $database = "s105710430_db"; 
-
-$conn = mysqli_connect($host, $username, $password, $database);
-
-
-if ($conn) {
-    echo "<p>connect successfully</p>";
-    mysqli_close($conn);
-}
-else{
-    echo "<p>connection failed</p>";
-}
 ?>
